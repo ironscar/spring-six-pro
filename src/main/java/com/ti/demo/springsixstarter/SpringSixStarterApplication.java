ce44ee42841af9ad60ef455ff9833e35cfb19d69
upamanyu <uchakravarty@ti.com>
2023-06-01 08:13:10 +0530

    init: initial commit

---
+++ src/main/java/com/ti/demo/springsixstarter/SpringSixStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ti.demo.springsixstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSixStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSixStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ti/demo/springsixstarter/SpringSixStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti.demo.springsixstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSixStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
